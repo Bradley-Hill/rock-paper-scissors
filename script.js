1c8888bd24631808309092790e213dd0032f5883
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-03 18:43:59 +0100

    ComputerSelection complete.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 console.log("Zehahahaha");
 let choiceNumber = Math.floor(Math.random() * (4 - 1)) + 1;
 console.log(choiceNumber);
+function getCompChoice(choiceNumber) {
+  if (choiceNumber == 1) {
+    console.log("Rock");
+  } else if (choiceNumber == 2) {
+    console.log("Paper");
+  } else if (choiceNumber == 3) {
+    console.log("Scissor");
+  }
+}
+getCompChoice(choiceNumber);
