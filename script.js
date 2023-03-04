970d754461c95bedbb65099e14631a1970a4ab36
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-04 17:29:45 +0100

    Script update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Zehahahaha");
 let choiceNumber = Math.floor(Math.random() * (4 - 1)) + 1;
-console.log(choiceNumber);
+
 function getCompChoice(choiceNumber) {
   if (choiceNumber == 1) {
     console.log("Rock");
