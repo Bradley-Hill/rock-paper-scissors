a9c9faf1427524621a1b1a444d1880fd21ad6d6f
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-04 17:47:09 +0100

    first go at function for game

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ let choiceNumber = Math.floor(Math.random() * (4 - 1)) + 1;
 
 function getCompChoice(choiceNumber) {
   if (choiceNumber == 1) {
+    compChoice = "Rock";
     console.log("Rock");
   } else if (choiceNumber == 2) {
+    compChoice = "Paper";
     console.log("Paper");
   } else if (choiceNumber == 3) {
-    console.log("Scissor");
+    compChoice = "Scissors";
+    console.log("Scissors");
+  }
+}
+function oneRound(compChoice, playerChoice) {
+  if (compChoice == playerChoice) {
+    console.log("Bad luck!That's a draw!");
   }
 }
 
