3aa27a2b743a99eb6c4e08b16265d812f300e17b
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-07 14:34:16 +0100

    Completed - rock-paper-scissors

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let score = 0;
+let compChoice = null;
+
 function getCompChoice() {
   let choiceNumber = Math.floor(Math.random() * (4 - 1)) + 1;
   if (choiceNumber == 1) {
@@ -21,20 +24,29 @@ function oneRound(compChoice, playerChoice) {
     console.log("Bad luck!That's a draw!");
   } else if (compChoice == "ROCK" && playerChoice == "PAPER") {
     console.log("Well done, Paper beats Rock!");
+    score++;
   } else if (compChoice == "SCISSOR" && playerChoice == "PAPER") {
-    console.log("Unlucky, Paper loses to Scissor!");
+    console.log("Unlucky, Paper loses to Scissor...");
   } else if (compChoice == "PAPER" && playerChoice == "ROCK") {
     console.log("Unlucky, Rock loses to Paper...");
   } else if (compChoice == "SCISSOR" && playerChoice == "ROCK") {
     console.log("Well done, Rock beats Scissors!");
+    score++;
   } else if (compChoice == "PAPER" && playerChoice == "SCISSOR") {
     console.log("Well done, Scissor beats Paper!");
+    score++;
   } else if (compChoice == "ROCK" && playerChoice == "SCISSOR") {
-    console.log("Unlucky, Scissor loses to Rock, you lose!");
+    console.log("Unlucky, Scissor loses to Rock, you lost...");
   }
 }
 
-var compChoice = getCompChoice();
-var playerChoice = getPlayerChoice();
-oneRound(compChoice, playerChoice);
-console.log(oneRound(compChoice, playerChoice));
+function game() {
+  for (let i = 0; i < 6; i++) {
+    getCompChoice();
+    let playerChoice = getPlayerChoice();
+    oneRound(compChoice, playerChoice);
+  }
+  console.log("Congratulations, your final score is: " + score);
+}
+
+game();
