dcb752d3ce3a8d29b9cc1896496fabf862c1fbe4
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-07 13:30:26 +0100

    oneRound function works.

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-console.log("Zehahahaha");
-let choiceNumber = Math.floor(Math.random() * (4 - 1)) + 1;
-
-function getCompChoice(choiceNumber) {
+function getCompChoice() {
+  let choiceNumber = Math.floor(Math.random() * (4 - 1)) + 1;
   if (choiceNumber == 1) {
-    compChoice = "Rock";
-    console.log("Rock");
+    compChoice = "ROCK";
   } else if (choiceNumber == 2) {
-    compChoice = "Paper";
-    console.log("Paper");
+    compChoice = "PAPER";
   } else if (choiceNumber == 3) {
-    compChoice = "Scissors";
-    console.log("Scissors");
+    compChoice = "SCISSOR";
   }
+  return compChoice;
 }
+
+function getPlayerChoice() {
+  var playerPrompt = prompt("Which do you choose, ROCK, PAPER or SCISSOR?");
+  var playerChoice = playerPrompt.toUpperCase();
+  return playerChoice;
+}
+
 function oneRound(compChoice, playerChoice) {
   if (compChoice == playerChoice) {
     console.log("Bad luck!That's a draw!");
+  } else if (compChoice == "ROCK" && playerChoice == "PAPER") {
+    console.log("Well done, Paper beats Rock!");
+  } else if (compChoice == "SCISSOR" && playerChoice == "PAPER") {
+    console.log("Unlucky, Paper loses to Scissor!");
+  } else if (compChoice == "PAPER" && playerChoice == "ROCK") {
+    console.log("Unlucky, Rock loses to Paper...");
+  } else if (compChoice == "SCISSOR" && playerChoice == "ROCK") {
+    console.log("Well done, Rock beats Scissors!");
+  } else if (compChoice == "PAPER" && playerChoice == "SCISSOR") {
+    console.log("Well done, Scissor beats Paper!");
+  } else if (compChoice == "ROCK" && playerChoice == "SCISSOR") {
+    console.log("Unlucky, Scissor loses to Rock, you lose!");
   }
 }
 
-var playerChoice = prompt("Which do you choose, Rock, Paper or Scissors?");
-
-getCompChoice(choiceNumber);
-console.log(playerChoice);
+var compChoice = getCompChoice();
+var playerChoice = getPlayerChoice();
+oneRound(compChoice, playerChoice);
+console.log(oneRound(compChoice, playerChoice));
